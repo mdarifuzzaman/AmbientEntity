0f53f3ff92eb3404a2a4265420a1db51c670a060
Md Arifuzzaman <arif_uap@outlook.com>
2019-02-10 18:31:05 +1100

    #add test

---
+++ Infrastructure.Test/ProductRepositoryTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Domain;
+using Infrastructure.Data;
+using Mehdime.Entity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Infrastructure.Test
+{
+    [TestClass]
+    public class ProductRepositoryTest
+    {
+        [TestMethod]
+        public void ProductShouldAddSuccessfullyToRepo()
+        {
+            var products = Moq.Mock.Of<IEntitySet<Product>>();
+            var mockFactory = new Mock<IDbContextScopeFactory>();
+            var mockScope = new Mock<IDbContextScope>();
+            bool saved = false;
+            mockScope.Setup(e => e.SaveChangesAsync()).Callback(()=> {
+                saved = true;
+            });
+            mockFactory.Setup(e => e.CreateWithTransaction(System.Data.IsolationLevel.Unspecified)).Returns(mockScope.Object);
+
+            var repo = new ProductRepository(products, mockFactory.Object);
+
+            var testProduct = new Product { ProductId = "test1234" };
+            repo.Add(testProduct);
+            Assert.IsTrue(saved);
+        }
+    }
+}
---
+++ Infrastructure.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Infrastructure.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Infrastructure.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b9e4469a-cefc-4fb3-bdac-98915442b3f8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
